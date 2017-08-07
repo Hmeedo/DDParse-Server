b946237f9d042930c8c75d384a5dba7979238995
Hmeedo <hmeedo@hotmail.co.il>
2017-08-07 21:02:56 +0300

    try

--- index.js
+++ index.js
@@ -1,59 +1,51 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
+
 var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-var path = require('path');
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
-var AzureStorageAdapter = require('parse-server-azure-storage').AzureStorageAdapter; 
+// var ParseServer = require('parse-server').ParseServer;
+// var path = require('path');
+// var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+// var AzureStorageAdapter = require('parse-server-azure-storage').AzureStorageAdapter; 
 
-if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
-}
+// if (!databaseUri) {
+//   console.log('DATABASE_URI not specified, falling back to localhost.');
+// }
 
- var account = process.env.STORAGE_ACCOUNT_NAME;		
- var container = process.env.STORAGE_CONTAINER_NAME;		
- var options = {		
-     accessKey: process.env.FILE_ACCESS_KEY || '',		
-     directAccess: true // If set to true, files will be served by Azure Blob Storage directly		
- }
+//  var account = process.env.STORAGE_ACCOUNT_NAME;		
+//  var container = process.env.STORAGE_CONTAINER_NAME;		
+//  var options = {		
+//      accessKey: process.env.FILE_ACCESS_KEY || '',		
+//      directAccess: true 
+//  }
  
-var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  maxUploadSize: process.env.MAX_UPLOAD_SIZE,
-  filesAdapter: new AzureStorageAdapter(account, container, options),
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
-});
-// Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
+// var api = new ParseServer({
+//   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+//   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+//   appId: process.env.APP_ID || 'myAppId',
+//   masterKey: process.env.MASTER_KEY || '', 
+//   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  
+//   maxUploadSize: process.env.MAX_UPLOAD_SIZE,
+//   filesAdapter: new AzureStorageAdapter(account, container, options),
+//   liveQuery: {
+//     classNames: ["Posts", "Comments"] 
+//   }
+// });
 
 var app = express();
 
-// Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
+// app.use('/public', express.static(path.join(__dirname, '/public')));
 
-// Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
-app.use(mountPath, api);
+// var mountPath = process.env.PARSE_MOUNT || '/parse';
+// app.use(mountPath, api);
 
-var mountPath = process.env.APP_MOUNT_PATH || '/parse';
-app.use(mountPath, api);
+// var mountPath = process.env.APP_MOUNT_PATH || '/parse';
+// app.use(mountPath, api);
 
-// Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
 app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+  //res.sendFile(path.join(__dirname, '/public/test.html'));
+   res.redirect('http://ec2-52-43-213-79.us-west-2.compute.amazonaws.com/parse');
 });
 
 var port = process.env.PORT || 1337;
@@ -62,5 +54,4 @@ httpServer.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
 
-// This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer);
+// ParseServer.createLiveQueryServer(httpServer);
